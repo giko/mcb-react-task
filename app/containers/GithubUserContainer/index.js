971ebe14fff925f9b4b7b0d7efd0e0253581aae3
kgrachev96 <kgrachev96@gmail.com>
2017-08-17 01:15:32 +0300

    Add "CommitContainer" and functionality...

--- app/containers/GithubUserContainer/index.js
+++ app/containers/GithubUserContainer/index.js
@@ -12,26 +12,63 @@ import { withState } from 'recompose';
 import makeSelectGithubUserContainer from './selectors';
 import * as actions from './actions';
 
+import CommitContainer from '../CommitContainer';
+
 const SearchComponentStateless = ({ searchText, setSearchText, loadUser }) => <div>
   <input type="text" value={searchText} onChange={(event) => setSearchText(event.target.value)} />
   <button onClick={() => loadUser(searchText)}>Search</button>
-</div>
+</div>;
 
 const SearchComponent = withState('searchText', 'setSearchText', 'giko')(SearchComponentStateless);
 
 export class GithubUserContainer extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      commits: [],
+    };
+  }
+
+  getCommits(title) {
+    fetch(
+      `https://api.github.com/repos/${this.user.state.stateValue}/${title}/commits`
+    )
+    .then((response) => response.json())
+    .then((result) =>
+    this.setState({ commits: result.slice(0, 10) }));
+  }
   render() {
     return (
       <div>
-        <SearchComponent loadUser={this.props.loadUser} />
-        {this.props.githubUserContainer.userRepos && this.props.githubUserContainer.userRepos.map((repo) => <div key={repo.id}>{repo.name}</div>)}
+        <SearchComponent
+          ref={(inputUser) => {
+            this.user = inputUser;
+          }}
+          loadUser={this.props.loadUser}
+        />
+        {this.props.githubUserContainer.userRepos &&
+          this.props.githubUserContainer.userRepos.map((repo) =>
+            <div key={repo.id}>
+              <button onClick={() => this.getCommits(repo.name)}>
+                {repo.name}
+              </button>
+            </div>)}
+        <CommitContainer commits={this.state.commits} />
       </div>
     );
   }
 }
 
+SearchComponentStateless.propTypes = {
+  searchText: PropTypes.string,
+  setSearchText: PropTypes.func,
+  loadUser: PropTypes.func,
+};
+
 GithubUserContainer.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  loadUser: PropTypes.func.isRequired,
+  githubUserContainer: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
