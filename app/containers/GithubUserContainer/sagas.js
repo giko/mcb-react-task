d2f8f108fb8ea17d313ea3a092a115c735eb5ebf
masambrero <31008566+masambrero@users.noreply.github.com>
2017-08-16 13:30:26 +0300

    Sagas changed
    holy damn...i hope that works!

--- app/containers/GithubUserContainer/sagas.js
+++ app/containers/GithubUserContainer/sagas.js
@@ -1,22 +1,38 @@
-import { take, call, put, select, takeLatest } from 'redux-saga/effects';
-import { request, postRequest } from 'utils/request';
+import { take, call, fork, put } from 'redux-saga/effects';
+import { request } from 'utils/request';
 
 import * as actions from './actions';
 
-// Individual exports for testing
 export function* loadUserSaga(action) {
   const result = yield call(request, `users/${action.payload.userName}/repos`);
   yield put(actions.userLoaded(result));
 }
 
-export function* defaultSaga() {
+export function* loadCommitsSaga(action) {
+  const result = yield call(request, `repos/${action.payload.repoName}/commits`);
+  const totalCommitsPerRequest = 10;
+  let totalResult = {};
+  Object.keys(result).filter((i, iIndex) => (iIndex < totalCommitsPerRequest)).forEach((j) => {
+    totalResult = [...totalResult, result[j]];
+  });
+  yield put(actions.commitsLoaded(totalResult));
+}
+
+function* watchLoadUser() {
   while (true) {
-    const action = yield take(actions.loadUser);
-    yield call(loadUserSaga, action);
+    const action = yield take('[2] Load user', actions.loadUser);
+    yield fork(loadUserSaga, action);
   }
 }
 
-// All sagas to be loaded
+function* watchLoadCommits() {
+  while (true) {
+    const action = yield take('[4] Load commits', actions.loadCommits);
+    yield fork(loadCommitsSaga, action);
+  }
+}
+
 export default [
-  defaultSaga,
+  watchLoadUser,
+  watchLoadCommits,
 ];
