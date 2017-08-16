94da3bd3f0fdf93af5d5a9e2d2d37b0270db641e
kgrachev96 <kgrachev96@gmail.com>
2017-08-17 00:33:11 +0300

    Add material-ui

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,6 +12,7 @@
  */
 
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
@@ -21,9 +22,11 @@ export default class App extends React.PureComponent { // eslint-disable-line re
 
   render() {
     return (
-      <div>
-        {React.Children.toArray(this.props.children)}
-      </div>
+      <MuiThemeProvider>
+        <div>
+          {React.Children.toArray(this.props.children)}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
