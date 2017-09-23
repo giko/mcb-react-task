c4b3bae9eb747e4d8bdeda9e168c546e4ec9e476
Eugene <31883189+Eugene1701@users.noreply.github.com>
2017-09-24 01:02:51 +0300

    styles add

---
+++ app/containers/GithubUserContainer/LeftListCol.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import {List} from 'material-ui/List';
+
+const LeftListCol = styled(List)`
+text-align: center;
+float:left;
+margin-left:25%;
+width: 20%;`;
+
+export default LeftListCol;
---
+++ app/containers/GithubUserContainer/ResultField.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const ResultField = styled.div`
+text-align: center;
+padding-top: 15px;`;
+
+export default ResultField;
